c9c1928f2b1c899013446c6091082c847fbd63a2
Alberto Soto <alberto.soto@gmail.com>
2019-12-13 17:01:12 +0100

    js sandbox

---
+++ 1-jsBasic-sandbox/src/index.js
@@ -0,0 +1,14 @@
+function shakeMe(){
+    alert("shake me baby!");
+}
+function setText(elem,headerText){
+    document.getElementById(elem).innerText =
+        headerText;
+}
+function createList(id,numItems) {
+    let txt = "";
+    for (let i = 0; i < numItems; i++) {
+        txt += "elem " + i + "-";
+    }
+    setText(id,txt);
+}
