d9c773b9d21c59cd5639244d616c26447f96a179
Alberto Soto <alberto.soto@gmail.com>
2020-01-31 16:27:43 +0100

    jquery cards

--- 3-npm-discovery/src/index.js
+++ 3-npm-discovery/src/index.js
@@ -1,26 +1,26 @@
 import $ from "jquery";
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
-/*
-Traditional js, basic, no lib!
- *//*
-console.log("yuhu");
-let aux = document.createElement("div");
-aux.innerHTML = "killing students!";
-document.getElementById("main").appendChild(aux);*/
-/*
- * Jquery way: 2005-2015
- *  $( document ).ready(function() {
-          // Handler for .ready() called.
-    });
- */
-console.log("yuhu");
-fetch("http://dummy.restapiexample.com/api/v1/employees")
-    .then(data => data.json())
-    .then(elem => {
-        console.log(elem);
-        elem.data.map(item => {
-            $("#main").append("<div>" + item.employee_name + "</div>");
+function printCard(name, salary) {
+    //employee_salary
+    return ("<div class='col-sm-3'>" + name + ":" + salary + "</div>");
+}
+
+$(document).ready(function () {
+    fetch("http://dummy.restapiexample.com/api/v1/employees")
+        .then(data => data.json())
+        .then(elem => {
+            console.log(elem);
+            let result = "";
+            elem.data.map(item => {
+                //result += printCard(item.employee_name, item.employee_salary);
+                result = result + printCard(item.employee_name, item.employee_salary);
+            });
+            $("#main").append(result);
         });
-    });
+    /*    $("button").click(function(){
+            $("p").slideToggle();
+        });*/
+});
+
+
 
